f49279e411845b8cde2877724fed4ccadb6905c3
savny004 <73206116+savny004@users.noreply.github.com>
2022-05-16 15:29:24 +0930

    Corrected study timer
    Previous version did not work with Expo and would cause issues down the line. 
    Changed to a different timer in order to fix issue

--- StudyTimer.js
+++ StudyTimer.js
@@ -1,81 +1,44 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
-import reactNativeBackgroundTimer from 'react-native-background-timer';
+import React from 'react';
+import { Text, View, StyleSheet, Button, TextInput, useState } from 'react-native';
+import Constants from 'expo-constants';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import numberInput from '../components/numberInput';
 
 
 const StudyTimer = ({navigation}) => {
+  const [isPlaying, setIsPlaying] = React.useState(false)
 
-  const [secondsLeft, setSecondsLeft] = useState(3601);
-  const [timerOn, setTimerOn] = useState(false);
 
-  useEffect(() => {
-    if(timerOn) startTimer();
-    else reactNativeBackgroundTimer.stopBackgroundTimer();
-    
-    return () => {
-      reactNativeBackgroundTimer.stopBackgroundTimer();
-    };
-  }, [timerOn]);
-
-  useEffect(() => {
-
-    if(secondsLeft === 0) 
-      reactNativeBackgroundTimer.stopBackgroundTimer();
-  }, [secondsLeft])
-
-  //Starts the timer and checks it each second. If running, it will reduce timer by 1 second.
-  const startTimer = () => {
-    reactNativeBackgroundTimer.runBackgroundTimer(() => {
-      setSecondsLeft(secs => {
-        if(secs > 0) return secs - 1
-        else return 0;
-      });
-    }, 1000);
-  };
-
-  // Takes remaining time and converts into a time format of HH:MM:SS
-  const clockify = () => {
-    let hours = Math.floor(secondsLeft / 60 / 60);
-    let mins = Math.floor(secondsLeft / 60 % 60);
-    let seconds = Math.floor(secondsLeft % 60);
-
-    let displayHours = hours < 10 ? '0${hours}' : hours;
-    let displayMins = mins < 10 ? '0${mins}' : mins;
-    let displaySeconds = seconds < 10 ? '0${seconds}' : seconds;
-
-    return {
-      displayHours, 
-      displayMins, 
-      displaySeconds
-  };
-};
-  
-
-return (
-  <View style = {styles.container}>
-    <Text style = {styles.time}>
-      {clockify().displayHours}Hours 
-      {clockify().displayMins}Mins 
-      {clockify().displaySeconds}Seconds
-      </Text>
-    <Button title= "Start/Stop" 
-    onPress = {() => setTimerOn((current) => !current)}> </Button>
+  return (
+    <View style={styles.container}>
+      <CountdownCircleTimer
+        isPlaying={isPlaying}
+        duration={10} //Enter time here
+       
+        colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+        colorsTime={[10, 6, 3, 0]}
+        onComplete={() => ({ shouldRepeat: false, delay: 2 })}
+    >
+      {({ remainingTime, color }) => (
+        <Text style={{ color, fontSize: 40 }}>
+          {remainingTime}
+        </Text>
+      )}
+    </CountdownCircleTimer>
+    <Button title="Start/Stop" onPress={() => setIsPlaying(prev => !prev)} />
   </View>
-);
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#000000',
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1, 
-  },
-  time: {
-    color: '#ffffff',
-    textAlign: 'center',
-    fontSize: 30,
-  },
-})
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  }
+});
 
 export default StudyTimer;
