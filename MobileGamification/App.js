04ec7085782eeeafdea82ba2fe7e33a49774eb10
xin-shang <69852053+xin-shang@users.noreply.github.com>
2022-05-18 03:59:43 +0930

    xin shang commit code add some expand package and React Native Navigation 18/5

--- MobileGamification/App.js
+++ MobileGamification/App.js
@@ -1,12 +1,26 @@
-
+// 
 import React from "react";
-import { View, Text } from "react-native";
 
-const Index = () => <View>
-  <Text>JSX</Text>
-  <Text>JSX</Text>
-  <Text>JSX</Text>
-  <Text>JSX</Text>
-</View>
+// 声明拓展件
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Provider as PaperProvider} from 'react-native-paper';
 
-export default Index;
+
+// 声明其他JS文件位置
+import HomeScreen from './screen/HomeScreen';
+import { ScreenStack } from "react-native-screens";
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <ScreenStack.Navigation>
+
+          <ScreenStack.Screen name = "Home" component = {HomeScreen} />
+
+        </ScreenStack.Navigation>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
---
+++ MobileGamification/screen/HomeScreen.js
@@ -0,0 +1,35 @@
+//
+import React from "react";
+
+// 声明拓展件
+import { ScrollView, StyleSheet } from 'react-native';
+import { Button, Card } from 'react-native-paper';
+//import { DefaultTheme } from 'react-native-paper';
+
+//
+function HomeScreen () {
+    return (
+        <ScrollView>
+            <Card>
+                <Card.Title title = "Home Screen" />
+            </Card>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create ({
+    //背景颜色，填充顶部10？
+    scrollView: {
+        backgroundColor: 'skyblue',
+        paddingTop: 10
+    },
+    // 左边界自动留白，右边界自动留白？
+    card: {
+        width: '90%',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    }
+});
+
+// 默认导出
+export default HomeScreen
