c27f60a08e56060573738d2d7953edc9050d6de8
xin-shang <69852053+xin-shang@users.noreply.github.com>
2022-05-18 01:52:53 +0930

    I init a expo again 2. Success

---
+++ MobileGamification/App.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { View, Text } from "react-native";
+
+const Index = () => <View>
+  <Text>JSX</Text>
+  <Text>JSX</Text>
+  <Text>JSX</Text>
+  <Text>JSX</Text>
+</View>
+
+export default Index;
