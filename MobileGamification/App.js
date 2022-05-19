6324f850964ca60942b95ceea08816647f74bdf3
xin-shang <69852053+xin-shang@users.noreply.github.com>
2022-05-19 21:19:28 +0930

    Xin Shang commit (19/5) - HomeScreen init, HomeScreen two button, Calender start a little bit

--- MobileGamification/App.js
+++ MobileGamification/App.js
@@ -1,26 +1,92 @@
-// 
-import React from "react";
+// // 
+// import React from "react";
 
-// 声明拓展件
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Provider as PaperProvider} from 'react-native-paper';
+// // 声明拓展件
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {Provider as PaperProvider} from 'react-native-paper';
+// import {ScreenStack} from "react-native-screens";
 
+// // 声明其他JS文件位置
+// import HomeScreen from './screen/HomeScreen';
 
-// 声明其他JS文件位置
-import HomeScreen from './screen/HomeScreen';
-import { ScreenStack } from "react-native-screens";
+// export default function App() {
+//   return (
+//     <PaperProvider>
+//       <NavigationContainer>
+//         <ScreenStack.Navigation>
 
-export default function App() {
+//           <ScreenStack.Screen name = "Home" component = {HomeScreen} />
+
+//         </ScreenStack.Navigation>
+//       </NavigationContainer>
+//     </PaperProvider>
+//   );
+// }
+
+//
+import * as React from 'react';
+
+//
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, Card } from 'react-native-paper';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <ScreenStack.Navigation>
-
-          <ScreenStack.Screen name = "Home" component = {HomeScreen} />
-
-        </ScreenStack.Navigation>
-      </NavigationContainer>
-    </PaperProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Mobile Gamification" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
+
+function HomeScreen () {
+    return (
+        <ScrollView>
+            <Card>
+                <Card.Title title = "Home Screen" />
+                <TouchableOpacity style = {styles.but}>
+                  <Text>Calendar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style = {styles.but}>
+                  <Text>Store</Text>
+                </TouchableOpacity>
+            </Card>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create ({
+    
+  //背景颜色，填充顶部10？
+    scrollView: {
+        backgroundColor: 'skyblue',
+        paddingTop: 10
+    },
+    
+    // 左边界自动留白，右边界自动留白？
+    card: {
+        width: '90%',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    
+    //按钮_1
+    but: {
+      width: "100%",
+      height: 60,
+      backgroundColor: "skyblue",
+      borderRadius: 10,
+      
+      //字体居中
+      justifyContent: "center",
+      alignItems: "center",
+    }
+});
--- MobileGamification/screen/HomeScreen.js
+++ MobileGamification/screen/HomeScreen.js
@@ -1,35 +1,36 @@
-//
-import React from "react";
+// //
+// import React from "react";
 
-// 声明拓展件
-import { ScrollView, StyleSheet } from 'react-native';
-import { Button, Card } from 'react-native-paper';
-//import { DefaultTheme } from 'react-native-paper';
+// // 声明拓展件
+// import { ScrollView, StyleSheet } from 'react-native';
+// import { Button, Card } from 'react-native-paper';
+// //import { DefaultTheme } from 'react-native-paper';
 
-//
-function HomeScreen () {
-    return (
-        <ScrollView>
-            <Card>
-                <Card.Title title = "Home Screen" />
-            </Card>
-        </ScrollView>
-    );
-}
+// //
+// function HomeScreen () {
+//     return (
+//         <ScrollView>
+//             <Card>
+//                 <Card.Title title = "Home Screen" />
+//             </Card>
+//         </ScrollView>
+//     );
+// }
 
-const styles = StyleSheet.create ({
-    //背景颜色，填充顶部10？
-    scrollView: {
-        backgroundColor: 'skyblue',
-        paddingTop: 10
-    },
-    // 左边界自动留白，右边界自动留白？
-    card: {
-        width: '90%',
-        marginLeft: 'auto',
-        marginRight: 'auto'
-    }
-});
+// const styles = StyleSheet.create ({
+//     //背景颜色，填充顶部10？
+//     scrollView: {
+//         backgroundColor: 'skyblue',
+//         paddingTop: 10
+//     },
+//     // 左边界自动留白，右边界自动留白？
+//     card: {
+//         width: '90%',
+//         marginLeft: 'auto',
+//         marginRight: 'auto'
+//     }
+// });
 
-// 默认导出
-export default HomeScreen
+// // 默认导出
+// export default HomeScreen
+
