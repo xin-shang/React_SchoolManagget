c1d5530a87ba7ca55c2c16b69d3e4a3351dd22bf
savny004 <73206116+savny004@users.noreply.github.com>
2022-06-06 16:55:41 +0930

    Agenda Calendar works
    Fixed issues where trying to call on Calendar and Agenda at the same time.

--- CalendarNick.js
+++ CalendarNick.js
@@ -1,91 +1,91 @@
-import React, {useState, Fragment, useCallback, useMemo} from 'react';
-import {StyleSheet, View, ScrollView, Text, TouchableOpacity} from 'react-native';
-import {Calendar, CalendarProps} from 'react-native-calendars';
-import testIDs from '../components/testIDs';
+import React, {useState} from 'react';
+import {View, TouchableOpacity, Text} from 'react-native';
+import {Agenda} from 'react-native-calendars';
+import {Card, Avatar} from 'react-native-paper';
 
 
-const INITIAL_DATE = '2022-01-01';
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split('T')[0];
+};
 
-const CalendarNick = ({navigation})  => {
+const CalendarNick = ({navigation}) => {
+ 
 
-  return(
-    
-    <Calendar
-      minDate={'2022-01-01'}
-      maxDate={'2022-12-31'}
-      firstDay={1}
-      enableSwipeMonths={true}
+  const [items, setItems] = useState({});
 
-      style={{
-        borderWidth: 1,
-        borderColor: 'gray',
-        height: 350
-      }}
-      theme={{
-        
-        textDayFontWeight: '300',
-        textMonthFontWeight: 'bold',
-        textDayHeaderFontWeight: '300',
-        textDayFontSize: 16,
-        textMonthFontSize: 16,
-        textDayHeaderFontSize: 16
-      }}
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Item for ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };9
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+        <Card>
+          <Card.Content>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              
+              <Text>{item.name}</Text>
+              
+            </View>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <Agenda
+        items = {{
+
+          '2022-06-09': [{ name: 'Presentation Day'}],
+          '2022-06-10': [{ name: 'Big Data Assignment Due'}],
+          '2022-06-24': [{ name: 'Design Patterns Exam'}],
+          '2022-12-25': [{ name: 'Christmas Day'}]
+
+        }
+          
+        }
+        //items={items}
+        loadItemsForMonth={loadItems}
+        selected={'2022-06-06'}
+        renderItem={renderItem}
+        markedDates={{
+          '2022-10-31': {selected:true, marked:true},
+          '2022-12-31': {marked:true},
+          '2022-04-25': {marked:true},
+          '2022-01-26': {marked: true}
+        }}
       />
-  )
+    </View>
+  );
+};
 
-}
-
-
-const styles = StyleSheet.create({
-  calendar: {
-    marginBottom: 10
-  },
-  switchContainer: {
-    flexDirection: 'row',
-    margin: 10,
-    alignItems: 'center'
-  },
-  switchText: {
-    margin: 10,
-    fontSize: 16
-  },
-  text: {
-    textAlign: 'center',
-    padding: 10,
-    backgroundColor: 'lightgrey',
-    fontSize: 16
-  },
-  disabledText: {
-    color: 'grey'
-  },
-  defaultText: {
-    color: 'purple'
-  },
-  customCalendar: {
-    height: 250,
-    borderBottomWidth: 1,
-    borderBottomColor: 'lightgrey'
-  },
-  customDay: {
-    textAlign: 'center'
-  },
-  customHeader: {
-    backgroundColor: '#FCC',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginHorizontal: -4,
-    padding: 8
-  },
-  customTitleContainer: {
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    padding: 10
-  },
-  customTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#00BBF2'
-  }
-});
 
 export default CalendarNick;
