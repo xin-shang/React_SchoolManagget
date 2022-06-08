77ef170a5c63c96c8fa2ad867076d275011733e6
BaoXiangPang <panby020@mymail.unisa.edu.au>
2022-06-08 10:23:18 +0930

    achievement screen, component

---
+++ components/AchievementCard.js
@@ -0,0 +1,27 @@
+import React, { Component, useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+import { Avatar } from 'react-native-paper';
+
+export default class AchiecvementCard extends Component {
+  
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      
+      return(
+        <Avatar.Icon style={styles.achievementIcon} size={100} icon={this.props.icon} backgroundColor={this.props.color}/>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    achievementIcon: {
+      borderRadius: 10,
+      
+    },
+   });
+  
+  AchiecvementCard.propTypes = { icon: PropTypes.string.isRequired, color: PropTypes.string.isRequired };
