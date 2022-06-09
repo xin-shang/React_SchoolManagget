e254718db0b849305907fc0df464f7e5d266cf8b
BaoXiangPang <panby020@mymail.unisa.edu.au>
2022-06-09 14:03:02 +0930

    layout changes

--- Screens/Home.js
+++ Screens/Home.js
@@ -10,13 +10,14 @@ const Home = ({ navigation }) => {
         flex:1
     }}>
         <Text style={styles.headings}>Don't Forget</Text>
-        <CourseCard title="INFT1020" body="Assignment on..." day="14" month="Apr" time="2359"/>
-        <CourseCard title="DATA6756" body="Quiz on..." day="16" month="Apr" time="2300"/>
-        <CourseCard title="DATA6756" body="Assignment on..." day="20" month="Apr" time="2359"/>
+        <View style={styles.cardWrapper}>
+            <CourseCard title="INFT1020" body="Assignment on..." day="14" month="Apr" time="2359"/>
+            <CourseCard title="DATA6756" body="Quiz on..." day="16" month="Apr" time="2300"/>
+            <CourseCard title="DATA6756" body="Assignment on..." day="20" month="Apr" time="2359"/>
+        </View>
 
         <View style={{
-            flex:.25,
-            marginTop: 10,
+            flex: 1,
             justifyContent: "flex-end"
             
         }}>
@@ -33,6 +34,10 @@ const styles = StyleSheet.create({
       fontSize: 30,
       color: "#fff",
     },
+    cardWrapper: {
+        alignItems: 'center'
+        
+    }
   });
 
 export default Home; 
